713e83f7b8c63e8785880a8658c8aaec7287e647
Yoseph Shibiru <deksyosyoseph@gmail.com>
2024-09-27 23:06:20 -0400

    center signin

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signin" element={
+          <div className='h-screen grid place-items-center'><SignIn /> </div> } />
         
         {/* Protect the /match route so only signed-in users can access it */}
         <Route path="/match" element={
